601917af985f1be1dd442e1aeb9b1ca7ca54857d
codivily <codivily@gmail.com>
2022-03-14 03:55:38 -0700

    4-puts.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of our program
+ *
+ * This is the main function. Where everything start.
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
